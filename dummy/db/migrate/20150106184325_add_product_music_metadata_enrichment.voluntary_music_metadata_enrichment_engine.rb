35ad7345463573ec1b88951ddbb01b0e5b2a0c93
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-01-09 13:24:32 +0100

    refs #1 Change music artists unique column from name to mbid.

--- dummy/db/migrate/20150106184325_add_product_music_metadata_enrichment.voluntary_music_metadata_enrichment_engine.rb
+++ dummy/db/migrate/20150106184325_add_product_music_metadata_enrichment.voluntary_music_metadata_enrichment_engine.rb
@@ -1,22 +1,13 @@
 # This migration comes from voluntary_music_metadata_enrichment_engine (originally 20150106183434)
 class AddProductMusicMetadataEnrichment < ActiveRecord::Migration
   def up
-    if product = Product.where(name: 'Music Metadata Enrichment').first
+    if Product.where(name: 'Music Metadata Enrichment').first
     else
       Product.create(name: 'Music Metadata Enrichment', text: 'Dummy') 
     end
     
-    create_table :music_genres do |t|
-      t.string :ancestry
-      t.string :name
-      t.timestamps
-    end
-    
-    add_index :music_genres, :ancestry
-    
     create_table :music_artists do |t|
       t.string :mbid
-      t.integer :genre_id
       t.string :name
       t.integer :listeners
       t.integer :plays
@@ -26,16 +17,13 @@ class AddProductMusicMetadataEnrichment < ActiveRecord::Migration
       t.timestamps
     end
     
-    add_index :music_artists, :genre_id
-    add_index :music_artists, :name, unique: true
+    add_index :music_artists, :mbid, unique: true
     
     create_table :music_releases do |t|
       t.string :mbid
-      t.integer :genre_id
       t.integer :artist_id
       t.string :artist_name
       t.string :name
-      t.integer :discs_count
       t.integer :tracks_count
       t.string :future_release_date
       t.datetime :released_at
@@ -49,34 +37,12 @@ class AddProductMusicMetadataEnrichment < ActiveRecord::Migration
     add_index :music_releases, :artist_id
     add_index :music_releases, [:artist_id, :name], unique: true
     
-    create_table :music_discs do |t|
-      t.string :mbid
-      t.integer :genre_id
-      t.integer :artist_id
-      t.string :artist_name
-      t.integer :release_id
-      t.string :release_name
-      t.integer :nr
-      t.string :name
-      t.integer :tracks_count
-      t.integer :listeners
-      t.integer :plays
-      t.string :state
-      t.timestamps
-    end
-    
-    add_index :music_discs, :release_id
-    add_index :music_discs, [:release_id, :name], unique: true
-    
     create_table :music_tracks do |t|
       t.string :mbid
-      t.integer :genre_id
       t.integer :artist_id
       t.string :artist_name
       t.integer :release_id
       t.string :release_name
-      t.integer :disc_id
-      t.string :disc_name
       t.integer :master_track_id
       t.integer :nr
       t.string :name
@@ -87,12 +53,11 @@ class AddProductMusicMetadataEnrichment < ActiveRecord::Migration
       t.timestamps
     end
     
-    add_index :music_tracks, :disc_id
-    add_index :music_tracks, [:disc_id, :name], unique: true
+    add_index :music_tracks, :release_id
+    add_index :music_tracks, [:release_id, :name], unique: true
     
     create_table :music_videos do |t|
       t.string :type
-      t.integer :genre_id
       t.integer :artist_id
       t.string :artist_name
       t.integer :track_id
@@ -111,12 +76,9 @@ class AddProductMusicMetadataEnrichment < ActiveRecord::Migration
   end
   
   def down
-    drop_table :music_genres
     drop_table :music_artists
     drop_table :music_releases
-    drop_table :music_discs
     drop_table :music_tracks
     drop_table :music_videos
-    drop_table :music_video_sources
   end
 end
