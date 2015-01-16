dcbaeabedaa4681c4d15a44fa15f636ff940ba9f
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-01-16 17:52:45 +0100

    refs #21 Adds bonus tracks import feature.

--- app/models/music_artist.rb
+++ app/models/music_artist.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 class MusicArtist < ActiveRecord::Base
   has_many :releases, class_name: 'MusicRelease', foreign_key: 'artist_id', dependent: :destroy
   has_many :tracks, class_name: 'MusicTrack', foreign_key: 'artist_id'
@@ -24,21 +25,60 @@ class MusicArtist < ActiveRecord::Base
       lastfm = Lastfm.new(LastfmApiKey, LastfmApiSecret)
       lastfm_artist = lastfm.artist.get_info(mbid: artist.mbid)
       artist.update_attributes(listeners: lastfm_artist['stats']['listeners'], plays: lastfm_artist['stats']['playcount'])
-  
-      musicbrainz_artist.release_groups.select{|r| ['Album', 'EP'].include?(r.type) && !r.secondary_types.include?('Live') && r.artists.length == 1}.each do |musicbrainz_release_group|
-        releases = musicbrainz_release_group.releases
+      
+      offset = 0
+      count = 100
+      
+      begin
+        release_groups = musicbrainz_artist.release_groups(offset: offset)
+        count = release_groups.total_count
         
-        next if releases.select{|r| r.status == 'Official' && !r.media.map(&:format).include?('DVD-Video') && !r.media.map(&:format).include?('DVD')}.none?
+        release_groups.select{|r| ['Album', 'EP'].include?(r.type) && r.secondary_types.select{|st| ['Compilation', 'Live', 'Remix'].include?(st)}.none? && r.artists.length == 1}.each do |musicbrainz_release_group|
+          releases = musicbrainz_release_group.releases
+          
+          next if releases.select{|r| r.status == 'Official' && !r.media.map(&:format).include?('DVD-Video') && !r.media.map(&:format).include?('DVD')}.none?
+          
+          release = MusicRelease.create(artist_id: artist.id, artist_name: artist.name, name: musicbrainz_release_group.title)
+          release.releases = releases
+          release.import_metadata!
+        end
         
-        release = MusicRelease.create(artist_id: artist.id, artist_name: artist.name, name: musicbrainz_release_group.title)
-        release.releases = releases
-        release.import_metadata!
-      end
+        offset += 100
+      end while offset < count  
+      
+      #artist.import_bonus_tracks
     end
   end
   
+  def import_bonus_tracks
+    offset = 0
+    count = 100
+    bonus_track_names = []
+    
+    begin
+      recordings = MusicBrainz::Recording.search(mbid, nil, limit: 100, offset: offset, create_models: true)
+      count = recordings.total_count
+      
+      recordings = recordings.select{|r| !['intro', 'outro'].include?(r.title.downcase) && !r.title.match(/\(/) && r.disambiguation.blank? }
+      voluntary_names = MusicTrack.where("artist_id = :artist_id AND LOWER(name) IN(:name)", artist_id: id, name: recordings.map(&:title).uniq.map(&:downcase)).map{|t| t.name.downcase }
+      
+      recordings.select{|r| !voluntary_names.include?(MusicTrack.format_name(r.title).downcase) }.each do |recording|
+        next if bonus_track_names.map(&:downcase).include? MusicTrack.format_name(recording.title).downcase
+        
+        track = MusicTrack.new(artist_id: id, name: MusicTrack.format_name(recording.title))
+        
+        if track.is_bonus_track?
+          track.create_bonus_track(recording.id)
+          bonus_track_names << track.name
+        end
+      end
+      
+      offset += 100
+    end while offset < count  
+  end
+  
   def bonus_tracks_release
-    releases.where(name: '[Bonus tracks]').first
+    releases.where(name: '[Bonus Tracks]').first
   end
   
   def musicbrainz_date_to_iso_date(date)
@@ -58,7 +98,7 @@ class MusicArtist < ActiveRecord::Base
   private
   
   def create_bonustracks_release
-    release = releases.create(name: '[Bonus tracks]')
+    release = releases.create(name: '[Bonus Tracks]')
     release.update_attribute(:state, 'active')
   end
   
