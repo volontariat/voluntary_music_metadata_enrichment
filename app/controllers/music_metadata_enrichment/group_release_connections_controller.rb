994eaf3089658082c1a663dcbe0a5c1bf8e915c3
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-01-20 19:51:25 +0100

    refs #27 Short artist name in releases list of group.

--- app/controllers/music_metadata_enrichment/group_release_connections_controller.rb
+++ app/controllers/music_metadata_enrichment/group_release_connections_controller.rb
@@ -8,7 +8,7 @@ class MusicMetadataEnrichment::GroupReleaseConnectionsController < ApplicationCo
       @group = MusicMetadataEnrichment::Group.find(params[:group_id])
       @year = params[:year]
       @releases = @group.releases.released_in_year(params[:year]).order('released_at DESC')
-      render partial: 'music_metadata_enrichment/releases/collection', locals: { paginate: false }, layout: false
+      render partial: 'music_metadata_enrichment/releases/collection', locals: { paginate: false, with_artist: true }, layout: false
     end
   end
 end
