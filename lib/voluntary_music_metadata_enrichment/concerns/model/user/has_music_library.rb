f2daa5957b46b57b0e3a1cffca47333918dc382d
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-01-21 18:05:44 +0100

    refs #12 Improves fault tolerance of music library artists importer and adds new parameter start_page to importer method. Don't try to create music library artist record for new user records.

--- lib/voluntary_music_metadata_enrichment/concerns/model/user/has_music_library.rb
+++ lib/voluntary_music_metadata_enrichment/concerns/model/user/has_music_library.rb
@@ -7,16 +7,21 @@ module VoluntaryMusicMetadataEnrichment
           
           included do
             has_many :music_library_artists
-            
+            has_many :music_artists, through: :music_library_artists, source: 'artist'
+            has_many :music_releases, through: :music_artists, source: 'releases'
+            has_many :music_tracks, through: :music_artists, source: 'tracks'
+                        
             scope :on_lastfm, -> { where('users.lastfm_user_name IS NOT NULL') }
           end
           
-          def import_music_artists(lastfm)
+          def import_music_artists(lastfm, start_page = 1)
             artist_names = []
             over_last_page = false
             
             1000.times do |page|
               page +=1
+              
+              next unless page >= start_page
               
               lastfm_artists = nil
               
@@ -32,13 +37,13 @@ module VoluntaryMusicMetadataEnrichment
                   end
                   
                   break
-                rescue Lastfm::ApiError
-                  puts "USER #{lastfm_user_name}: LIBRARY PAGE ##{page} ... Lastfm::ApiError ... TRY AGAIN"
+                rescue Lastfm::ApiError, Timeout::Error => e
+                  puts "USER #{lastfm_user_name}: LIBRARY PAGE ##{page} ... #{e.class.name}... TRY AGAIN"
                   sleep 30
                 end
               end
              
-              if lastfm_artists.first.nil?
+              if lastfm_artists.nil? || lastfm_artists.first.nil?
                 puts "USER #{lastfm_user_name}: LIBRARY PAGE ##{page} IS EMPTY" 
                 over_last_page = true
                 break
@@ -68,7 +73,7 @@ module VoluntaryMusicMetadataEnrichment
                 end
                 
                 if artist
-                  music_library_artists.create(artist_id: artist.id, plays: lastfm_artist['playcount'])
+                  music_library_artists.create(artist_id: artist.id, plays: lastfm_artist['playcount']) unless new_record?
                 end
               end
               
