430b89c6c889dc2308fb24392723aa7c8e8232bb
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-01-23 13:39:52 +0100

    #38 #22, #23 Authorization for managing years in review music.

--- lib/voluntary_music_metadata_enrichment/ability.rb
+++ lib/voluntary_music_metadata_enrichment/ability.rb
@@ -3,12 +3,15 @@ module VoluntaryMusicMetadataEnrichment
     def self.after_initialize
       Proc.new do |ability, user, options|
         ability.can :read, [
-          MusicMetadataEnrichment::Group, MusicMetadataEnrichment::GroupArtistConnection, MusicArtist, MusicRelease, MusicTrack, MusicVideo
+          MusicMetadataEnrichment::Group, MusicMetadataEnrichment::GroupArtistConnection, YearInReviewMusic, YearInReviewMusicRelease, YearInReviewMusicTrack, MusicArtist, MusicRelease, MusicTrack, MusicVideo
         ]
         
         if user.present?
           ability.can(:create, MusicMetadataEnrichment::Group)
           ability.can([:create, :name_confirmation, :select_artist, :creation], MusicMetadataEnrichment::GroupArtistConnection)
+          ability.can(:create, YearInReviewMusic)
+          ability.can(:create, YearInReviewMusicRelease) {|year_in_review_music_release| year_in_review_music_release.year_in_review_music.user_id == user.id }
+          ability.can(:create, YearInReviewMusicTrack) {|year_in_review_music_track| year_in_review_music_track.year_in_review_music.user_id == user.id }
           ability.can([:create, :name_confirmation], MusicArtist)
           ability.can([:create, :artist_confirmation, :select_artist, :name, :name_confirmation, :announce, :create_announcement], MusicRelease)
           ability.can([:create, :artist_confirmation, :select_artist, :name, :name_confirmation], MusicTrack)
