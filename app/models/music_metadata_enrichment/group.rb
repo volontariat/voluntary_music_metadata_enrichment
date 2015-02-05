286dbae892c34b21c66555f493f39ccfe73601fe
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-02-05 15:39:36 +0100

    refs #15 Implements MusicMetadataEnrichment::Group#import_artists_of_group_members.

--- app/models/music_metadata_enrichment/group.rb
+++ app/models/music_metadata_enrichment/group.rb
@@ -49,6 +49,76 @@ module MusicMetadataEnrichment
       artist_names_without_mbid
     end
     
+    def import_artists_of_group_members(options = {})
+      last_page = options[:last_page]
+      last_user_name_from_last_run = options[:last_user_name_from_last_run]
+      library_start_page = options[:library_start_page]
+      
+      user_names, page, last_user_name, last_user_name_from_last_run_reached, i = [], nil, nil, false, 1
+      
+      lastfm = Lastfm.new(LastfmApiKey, LastfmApiSecret)
+      
+      1000.times do |page|
+        page +=1
+        
+        if last_page.present? && page == last_page
+          i = (page * 50) + 1
+        elsif last_page.present? && page < last_page
+          next
+        end
+      
+        puts "GROUP MEMBERS PAGE: #{page}"
+        
+        lastfm_users = nil
+        
+        3.times do
+          begin
+            lastfm_users = lastfm.group.get_members(group: 'Dark Electro', page: page)
+            
+            break
+          rescue Lastfm::ApiError
+            puts "GROUP MEMBERS PAGE: #{page} ... TRY AGAIN"
+            sleep 30
+          end
+        end
+        
+        if lastfm_users.select{|u| !user_names.include?(u['name'])}.none?
+          # over last page
+          break
+        end
+      
+        lastfm_users.each do |lastfm_user|
+          if user_names.include?(lastfm_user['name'])
+            next
+          else
+            user_names << lastfm_user['name']
+          end
+      
+          if lastfm_user['name'] == last_user_name_from_last_run
+            last_user_name_from_last_run_reached = true
+          elsif last_user_name_from_last_run.present? && !last_user_name_from_last_run_reached
+            i += 1
+            next
+          end
+          
+          last_user_name = lastfm_user['name']
+          puts "USER #{i}: #{last_user_name} (GROUP MEMBERS PAGE: #{page})"
+          user = User.new
+          user.lastfm_user_name = lastfm_user['name']
+      
+          if lastfm_user['name'] == last_user_name_from_last_run && !library_start_page.nil?
+            user.import_music_artists(lastfm, library_start_page)
+          else
+            user.import_music_artists(lastfm)
+          end
+          
+          i += 1
+        end
+        
+        [page, last_user_name]
+      end
+    end
+    
     private
     
     def registered_on_lastfm
