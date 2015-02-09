ec744eca04806f6604a25b3a897079be2c5c0be6
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-02-09 09:29:53 +0100

    year in review music releases and tracks will be deleted instead of destroyed after year in review music will be destroyed.

--- app/models/year_in_review_music.rb
+++ app/models/year_in_review_music.rb
@@ -3,8 +3,8 @@ class YearInReviewMusic < ActiveRecord::Base
   
   belongs_to :user
   
-  has_many :releases, class_name: 'YearInReviewMusicRelease', dependent: :destroy
-  has_many :tracks, class_name: 'YearInReviewMusicTrack', dependent: :destroy
+  has_many :releases, class_name: 'YearInReviewMusicRelease', dependent: :delete_all
+  has_many :tracks, class_name: 'YearInReviewMusicTrack', dependent: :delete_all
   
   validates :user_id, presence: true
   validates :year, presence: true, numericality: { only_integer: true }, uniqueness: { scope: :user_id }
