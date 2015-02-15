902c151cb6848733e69e8a0f457df98a6cbf171d
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-02-15 17:26:32 +0100

    Removes test code.

--- app/controllers/product/music_metadata_enrichment_controller.rb
+++ app/controllers/product/music_metadata_enrichment_controller.rb
@@ -2,7 +2,5 @@ class Product::MusicMetadataEnrichmentController < ApplicationController
   include ::MusicMetadataEnrichment::BaseController
   
   def index
-    MusicRelease.find(190).set_spotify_album_id
-    raise 'done'
   end
 end
