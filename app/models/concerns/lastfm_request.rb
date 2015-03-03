b007f32db97dd67abaa802d2bb96ce4edc6ac0f1
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-03 11:08:00 +0100

    Handle lastfm api errors where message is 'Operation failed'.

--- app/models/concerns/lastfm_request.rb
+++ app/models/concerns/lastfm_request.rb
@@ -13,34 +13,39 @@ module LastfmRequest
       raise_if_response_is_just_nil = params.delete(:raise_if_response_is_just_nil)
       raise_parse_exception = params.delete(:raise_parse_exception)
       
-      begin
-        i = 1
+      i = 1
         
-        3.times do
-          begin
-            response = lastfm.send(resource).send(method, params)
-            
-            raise 'last.fm response is just nil without exceptions' if response.nil? && raise_if_response_is_just_nil
-            
-            break
-          rescue Timeout::Error, EOFError => e 
-            puts "lastfm: #{e.class.name} for ##{i} time"
-            sleep 60 
-          rescue ArgumentError => e
-            if e.message.match('File does not exist')
-              puts "lastfm: 'File does not exist' sleep 60 seconds for ##{i} time ..."
+      3.times do
+        begin
+          response = lastfm.send(resource).send(method, params)
+          
+          raise 'last.fm response is just nil without exceptions' if response.nil? && raise_if_response_is_just_nil
+          
+          break
+        rescue Timeout::Error, EOFError => e 
+          puts "lastfm: #{e.class.name} for ##{i} time"
+          sleep 60 
+        rescue ArgumentError => e
+          if e.message.match('File does not exist')
+            puts "lastfm: 'File does not exist' sleep 60 seconds for ##{i} time ..."
+            sleep 60
+          else
+            raise e
+          end
+        rescue Lastfm::ApiError => e
+          if error_message_reqexp.nil? || e.message.match(error_message_reqexp).nil?
+            if e.message.match(/Operation failed - Something else went wrong/)
+              puts "lastfm: operation failed - something else went wrong for ##{i} time ..."
               sleep 60
             else
               raise e
             end
           end
-          
-          i += 1
+        rescue REXML::ParseException => e
+          raise e if raise_parse_exception
         end
-      rescue Lastfm::ApiError => e
-        raise e unless !error_message_reqexp.nil? && e.message.match(error_message_reqexp)
-      rescue REXML::ParseException => e
-        raise e if raise_parse_exception
+        
+        i += 1
       end
       
       response
