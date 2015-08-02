e88f8faceaa87e6c0d962baff45063526dbe1d6a
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-02 13:36:39 +0200

    Removes fork me on GitHub links.

--- app/controllers/music_metadata_enrichment/application_controller.rb
+++ app/controllers/music_metadata_enrichment/application_controller.rb
@@ -8,8 +8,4 @@ class MusicMetadataEnrichment::ApplicationController < ::ApplicationController
   def voluntary_application_javascripts
     ['voluntary/application', 'voluntary_music_metadata_enrichment/application', 'application'] 
   end
-  
-  def voluntary_application_repository_path
-    'volontariat/voluntary_music_metadata_enrichment'
-  end
 end
